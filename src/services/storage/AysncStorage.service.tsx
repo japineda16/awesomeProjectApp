cad88b2290a16dc2ffb1d4d42a1e2e2515b196b2
Jose Pineda <pinedaborges.josealejandro@gmail.com>
2022-11-18 17:53:59 -0400

    deleted alert "save"

--- src/services/storage/AysncStorage.service.tsx
+++ src/services/storage/AysncStorage.service.tsx
@@ -2,7 +2,6 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export const saveData = async (key: string, data: any) => {
     try {
-        alert('saveee');
         return await AsyncStorage.setItem(key, data);
     } catch (e) {
       alert('No se ha podido almacenar la informacion');
