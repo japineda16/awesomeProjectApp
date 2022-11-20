3bf3d41b3e9c95d543392fc68481d0ee8b88d600
Jose Pineda <pinedaborges.josealejandro@gmail.com>
2022-11-20 17:36:04 -0400

    Added toast to clients

--- src/ClientList/component/modelClient.tsx
+++ src/ClientList/component/modelClient.tsx
@@ -1,11 +1,11 @@
-import { FormControl, Input, Modal, ScrollView, Text } from "native-base";
+import { Box, FormControl, Input, Modal, ScrollView, Text, useToast } from "native-base";
 import { useEffect, useState } from "react";
 import { Keyboard, StyleSheet, Alert } from "react-native";
 import { getQuery, postQuery } from "../../services/query/query.service";
 import { ClientForm } from "./clientForm";
 
 export const ModalClient = ({status, onClose, onSubmit}) => {
-
+    const toast = useToast();
     const [position, setPosition] = useState('center');
     const [isLoading, setLoading] = useState(false);
 
@@ -34,6 +34,16 @@ export const ModalClient = ({status, onClose, onSubmit}) => {
         const {data} = await postQuery('clients', form).catch((err) => {
             setLoading(false);
             Alert.alert('Error', 'Ha sucedido un error, por favor vuelva a intentarlo.');
+        });
+        toast.show({
+            render: () => {
+                return (
+                <Box backgroundColor='success.500' px={4} py={3}>
+                    <Text color='white'>Se ha agregado exitosamente el cliente.</Text>
+                </Box>
+                )
+
+            }
         });
         setLoading(false);
         onSubmit(data);
